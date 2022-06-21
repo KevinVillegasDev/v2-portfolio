2dfcb7189ee9b81626683765aa3f57d6f07ea63a
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-20 20:29:13 -0700

    added more icons for contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,25 @@
 const Contact = () => {
-    return <div className="contact">contact</div>;
+    return (
+        <div className="contact">
+            <h2>Feel free to contact me through email or LinkedIn.</h2>
+            <h2>I look forward to hearing from you!</h2>
+            <div className="contacticons">
+                <a
+                    href="mailto:kevin.villegas94@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src="/email.png" />
+                </a>
+                <a href="https://www.linkedin.com/in/kevinvillegasdev/">
+                    <img src="/linked.png" />
+                </a>
+                <a href="https://github.com/KevinVillegasDev">
+                    <img src="/github.png" />
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,11 @@ const Home = () => {
     return (
         <div className="home">
             <h1>Kevin Villegas</h1>
+            <p>
+                Full Stack Developer from Los Angeles with a passion for
+                technology, DeFi and the outdoors.
+            </p>
+            <img src="/selfie.JPG" />
         </div>
     );
 };
